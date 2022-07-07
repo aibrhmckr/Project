2d623afebe79a0db6de1ae28c9de5d3ce2e0beab
Ali İbrahim ÇAKIR <aibrhmckrofficial@gmail.com>
2022-07-07 09:17:08 +0300

    Signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,32 +1,42 @@
+import { useDispatch } from "react-redux";
 import "./Signup.css"
+import { name,surname,epost,password,save } from "./stores/Log";
+import { useState } from "react";
 
 const Signup = () => {
+  const dispatch = useDispatch();
+  const[Name,setName]=useState("");
+  const[SurName,setSurname]=useState("");
+  const [Epost ,setEpost]=useState("");
+  const [Password,setPassword]=useState("");
 
   return (
     <div className="user_info">
-      <form>
+
         <div>
           <b>Ad</b>
-          <input  className="text-field"/>
+          <input  className="text-field" onChange={(e)=>{setName(e.target.value)}}/>
         </div>
         <div >
           <b>Soyad</b>
-          <input className="text-field" />
+          <input className="text-field" onChange={(e)=>{setSurname(e.target.value)}}/>
         </div>
         <div >
           <b>E-Posta</b>
-          <input className="text-field"/>
+          <input className="text-field" onChange={(e) => {setEpost(e.target.value)}}/>
         </div>
 
         <div >
           <b>Şifre</b>
-          <input className="text-field"/>
+          <input className="text-field" onChange={(e) => {setPassword(e.target.value)}} />
         </div>
         <button className="signup_button" onClick={()=>{
-
+          dispatch(name(Name))
+          dispatch(surname(SurName))
+          dispatch(epost(Epost))
+          dispatch(password(Password))
+          dispatch(save())
         }}>Kaydol</button>
-
-      </form>
     </div>
   );
 };
--- src/components/stores/Log.js
+++ src/components/stores/Log.js
@@ -1,40 +1,57 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 export const logSlice = createSlice({
-  name: 'counter',
+  name: "counter",
   initialState: {
     value: {
-        epost: "",
-        password: "",
-        name: "",
-        surname: ""
+      epost: "",
+      password: "",
+      name: "",
+      surname: "",
     },
   },
   reducers: {
-    epost:(state,action)=>{
-      state.value.epost= action.payload
-      console.log(state.value.epost)
-
+    epost: (state, action) => {
+      state.value.epost = action.payload;
+      console.log(state.value.epost);
     },
-    password:(state,action)=>{
-      state.value.password= action.payload
-      console.log(state.value.password)
-
+    password: (state, action) => {
+      state.value.password = action.payload;
+      console.log(state.value.password);
     },
-    logging:(state)=>{
-      if(state.value.epost=="alicakir" && state.value.password){
-        console.log("Giriş yapıldı")
+    name: (state, action) => {
+      state.value.name = action.payload;
+    },
+    surname: (state, action) => {
+      state.value.surname = action.payload;
+    },
+    logging: (state) => {
+      if (
+        state.value.epost === "alicakir" &&
+        state.value.password === "12345678"
+      ) {
+        console.log("Giriş yapıldı");
+      } else {
+        console.log("Kullanıcı adı veya parola hatalı");
       }
-      else{
-        console.log("Kullanıcı adı veya parola hatalı")
+    },
+    save: (state) => {
+      if (
+        state.value.epost !== "" &&
+        state.value.password !== "" &&
+        state.value.name !== "" &&
+        state.value.surname !== ""
+      ) {
+        console.log("Kaydedildi");
+      } else {
+        console.log("Lütfen tüm alanları doldurun");
       }
-    }
-
-    
+    },
   },
-})
+});
 
 // Action creators are generated for each case reducer function
-export const {logging,epost,password} = logSlice.actions
+export const { logging, epost, password, name, surname, save } =
+  logSlice.actions;
 
-export default logSlice.reducer
+export default logSlice.reducer;
