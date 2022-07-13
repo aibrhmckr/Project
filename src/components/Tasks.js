f140cce956fdbfecb6698ac54438602e28d718ea
Ali İbrahim ÇAKIR <aibrhmckrofficial@gmail.com>
2022-07-13 17:53:47 +0300

    mui güncelleme

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,13 +1,17 @@
 import { IconButton } from "@mui/material";
+import { Button } from "semantic-ui-react";
 
-const Tasks=()=>{
-    return(
-        <div>
-            <div className="kart">
-                Görev
-            </div>
-
-        </div>
-    )
-}
+const Tasks = () => {
+  return (
+    <div>
+      <div className="kart">
+        Görev<Button.Group>
+          <Button icon="play" />
+          <Button icon="check" />
+          <Button icon="edit" />
+        </Button.Group>
+      </div>
+    </div>
+  );
+};
 export default Tasks;
