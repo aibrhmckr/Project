630e3ee58d43c16c7e64f93ec911b60058f0e439
Ali İbrahim ÇAKIR <aibrhmckrofficial@gmail.com>
2022-07-13 17:40:19 +0300

    mui kurulmadan önce

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,12 +1,12 @@
 import { IconButton } from "@mui/material";
-import DeleteIcon from '@mui/icons-material/Delete';
 
 const Tasks=()=>{
     return(
         <div>
-            <IconButton>
-                <DeleteIcon/>
-            </IconButton>
+            <div className="kart">
+                Görev
+            </div>
+
         </div>
     )
 }
