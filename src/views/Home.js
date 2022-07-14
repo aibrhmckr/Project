9e272d70c83ad64556bc9e97ae1cd32d9cdc1d01
Ali İbrahim ÇAKIR <aibrhmckrofficial@gmail.com>
2022-07-14 11:32:51 +0300

    db json a yazma ve getirme işlemleri yapılıo

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,5 +1,5 @@
 import { TextField } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../components/stores/Log";
 import Tasks from "../components/Tasks";
@@ -10,8 +10,47 @@ const Home = ({ setIsLog }) => {
 
   const [text,setText]=useState("");
 const kullanici = localStorage.getItem("epost")+"#TR28"
-    let task=[
-    ]
+    let task=[]
+
+    const [departments,setDepartments] = useState([])
+    useEffect(() => {
+      fetchData();
+    }, [])
+    console.log(departments)
+
+const fetchData=async()=>{
+  await fetch("http://localhost:3004/departments")
+  .then((response) => response.json())
+  .then((data) => setDepartments(data))
+  .catch((error) => console.log(error));
+}
+const onAdd = async (id, title) => {
+  await fetch("http://localhost:3004/departments", {
+    method: "POST",
+    body: JSON.stringify({
+      id: 10,
+      title: "Okul"
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+    .then((response) => {
+      if (response.status !== 201) {
+        return;
+      } else {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      fetchData()
+    })
+    .catch((error) => console.log(error));
+};
+
+
+
+
   return (
     <div>
       <button
@@ -34,6 +73,7 @@ const kullanici = localStorage.getItem("epost")+"#TR28"
                 localStorage.removeItem(kullanici)
                 localStorage.setItem(kullanici,task)
                 console.log(localStorage.getItem(kullanici))
+                onAdd()
             }
         }}> Add </button>
       </div>
