44139149ba6995c4096d76537d5f60e0af3bae98
Ali İbrahim ÇAKIR <aibrhmckrofficial@gmail.com>
2022-07-25 17:37:24 +0300

    Uygulama her şeyiyle işlevsellik olarak bitti

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 //import { useSelector } from "react-redux";
 
 import { logging, epost, password } from "./stores/Log";
@@ -17,51 +17,72 @@ const Signin = ({ setIsLog }) => {
   const [Password, setPassword] = useState("");
   const dispatch = useDispatch();
 
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetchData();
+  });
+  const fetchData = async () => {
+    await fetch("http://localhost:3004/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data))
+      .catch((error) => console.log(error));
+  };
+
   const LoginHandler = () => {
     dispatch(epost(Epost));
     dispatch(password(Password));
-    const user = Users.find(
+    /*const user = Users.find(
       (user) =>
         user.epost === Epost &&
         user.password === Password &&
-        (Epost !== "") && (Password !== "")
+        Epost !== "" &&
+        Password !== ""
     );
     if (user) {
       setIsLog("true");
       dispatch(logging());
+    }*/
+    let finder = false;
+    users.forEach((user) => {
+      if (user.email === Epost) finder = true;
+    });
+    if (finder) {
+      //console.log(user)
+      setIsLog("true");
+      dispatch(logging());
+      //Users.push(newUser);image.png
     }
   };
   //22.07.2022
-  const [locale,setLocale]=useState(localStorage.getItem("locale"))
+  const [locale, setLocale] = useState(localStorage.getItem("locale"));
 
   return (
     <I18nProvider locale={locale}>
-    <div className="user_info">
-      
-      <div>
-        <b>{translate("email")}</b>
-        <input
-          className="e-post__field"
-          onChange={(e) => {
-            setEpost(e.target.value);
-          }}
-        />
-      </div>
+      <div className="user_info">
+        <div>
+          <b>{translate("email")}</b>
+          <input
+            className="e-post__field"
+            onChange={(e) => {
+              setEpost(e.target.value);
+            }}
+          />
+        </div>
 
-      <div>
-        <b>{translate("password")}</b>
-        <input
-          className="password__field"
-          type="password"
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
+        <div>
+          <b>{translate("password")}</b>
+          <input
+            className="password__field"
+            type="password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </div>
+        <button className="login_button" onClick={LoginHandler}>
+          <b>{translate("signin-button")}</b>
+        </button>
       </div>
-      <button className="login_button" onClick={LoginHandler}>
-        <b>{translate("signin-button")}</b>
-      </button>
-    </div>
     </I18nProvider>
   );
 };
