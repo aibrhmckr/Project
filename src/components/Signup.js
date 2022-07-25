238b2240180efb92c0f2668fe28f75e9b96c80a3
Ali İbrahim ÇAKIR <aibrhmckrofficial@gmail.com>
2022-07-25 17:37:29 +0300

    uygulama her şeyiyle işlevsellik olarak bitti

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,7 @@
 import { useDispatch } from "react-redux";
 import "./Signup.css";
 import { name, surname, epost, password, save } from "./stores/Log";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { Users } from "../api/gecici";
 import { I18nProvider, LOCALES } from "./i18n";
 import translate from "./i18n/translate";
@@ -13,34 +13,79 @@ const Signup = ({ setIsLog }) => {
   const [Epost, setEpost] = useState("");
   const [Password, setPassword] = useState("");
 
+  const [users, setUsers] = useState([]);
+  useEffect(()=>{
+fetchData()
+  })
+
+  const fetchData=async()=>{
+    await fetch("http://localhost:3004/users")
+    .then((response)=>response.json())
+    .then((data)=>setUsers(data))
+    .catch((error)=>console.log(error))
+  }
+  const onAdd = async (name,surname, email,password) => {
+    await fetch("http://localhost:3004/users", {
+      method: "POST",
+      body: JSON.stringify({
+        name: name,
+        surname:surname,
+        email: email,
+        password:password
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
+      .then((response) => {
+        if (response.status !== 201) {
+          return;
+        } else {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        setUsers((users) => [...users, data]);
+      })
+      .catch((error) => console.log(error));
+  };
+
   const clickHandler = () => {
-    const newUser = {
+   /* const newUser = {
       epost: Epost,
       password: Password,
       name: Name,
       surname: SurName,
-    };
+    };*/
     dispatch(epost(Epost));
     dispatch(password(Password));
     dispatch(name(Name));
     dispatch(surname(SurName));
-    const user = Users.find(
+
+   /* 
+    const user = users.find(
       (user) =>
-        user.epost !== Epost &&
+        user.email !== Epost &&
         Epost !== "" &&
         Password !== "" &&
         Name !== "" &&
         SurName !== ""
-    );
-    if (user) {
+    );*/
+    let finder=false;
+    users.forEach(user => {
+      if(user.email===Epost)finder=true
+    });
+    if (!finder) {
+      //console.log(user)
       setIsLog("true");
       dispatch(save());
-      Users.push(newUser);
+      //Users.push(newUser);
+      onAdd(Name,SurName,Epost,Password)
     }
   };
   //22.07.2022
   const [locale,setLocale]=useState(localStorage.getItem("locale"))
-
+//
   return (
     <I18nProvider locale={locale}>
     <div className="user_info">
